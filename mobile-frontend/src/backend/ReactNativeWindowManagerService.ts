f19fe557941b612fb8b377fa64ea5c9e9d51328c
0o-de-lally <1364012+0o-de-lally@users.noreply.github.com>
2025-01-01 15:22:31 +0100

    wip remove backend

--- mobile-frontend/src/backend/ReactNativeWindowManagerService.ts
+++
@@ -1,5 +0,0 @@
-import { WindowManagerService } from "@postero/core";
-
-class ReactNativeWindowManagerService implements WindowManagerService {}
-
-export default ReactNativeWindowManagerService;
--- mobile-frontend/src/backend/browser-link/ReactNativeBrowserLinkService.ts
+++
@@ -1,6 +0,0 @@
-import { Injectable } from "@nestjs/common";
-
-@Injectable()
-class ReactNativeBrowserLinkService {}
-
-export default ReactNativeBrowserLinkService;
