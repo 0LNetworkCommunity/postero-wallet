c58dc221895133c1fdce8a148416ff2243523dfc
0o-de-lally <1364012+0o-de-lally@users.noreply.github.com>
2025-02-01 16:21:48 -0500

    update eslint in ui

--- ui/lib/types/emotion/types.d.ts
+++ ui/lib/types/emotion/types.d.ts
@@ -14,9 +14,9 @@ declare module "@emotion/react" {
   }
 }
 
-// You are also able to use a 3rd party theme this way:
-import "@emotion/react";
-import { LibTheme } from "some-lib";
+// // You are also able to use a 3rd party theme this way:
+// import "@emotion/react";
+// import { LibTheme } from "some-lib";
 
 declare module "@emotion/react" {
   export interface Theme extends LibTheme {}
